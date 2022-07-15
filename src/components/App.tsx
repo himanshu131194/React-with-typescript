6fbb68c70650337e419d5a1ffd71a2831db91e1f
Himanshu Savita <himanshu131194@gmail.com>
2022-07-15 13:24:33 +0530

    ncu updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,97 +24,141 @@
 
 // interface State {
 //     userName: string,
-//     imageUrl: string 
+//     imageUrl: string
 // }
 
-// const App: React.FC = ()=>{
-//    const [ state, setState ] = useState<State>({
-//       userName: 'John',
-//       imageUrl: 'https://picsum.photos/600/600'
-//    });
+// const App: React.FC = () => {
+//   const [state, setState] = useState<State>({
+//     userName: "John",
+//     imageUrl: "https://picsum.photos/600/600",
+//   });
 
-//    const myStyles = {
-//       margin: 40
-//    }
-//    return <div style={myStyles} onClick={()=>setState({userName: 'John',
-//    imageUrl: 'https://picsum.photos/600/600'})}>
-//             <h1>{state.userName}</h1>
-//             <img src={state.imageUrl}/>
-//          </div>
-
-// }
+//   const myStyles = {
+//     margin: 40,
+//   };
+//   return (
+//     <div
+//       style={myStyles}
+//       onClick={() =>
+//         setState({
+//           userName: "John",
+//           imageUrl: "https://picsum.photos/600/600",
+//         })
+//       }
+//     >
+//       <h1>{state.userName}</h1>
+//       <img src={state.imageUrl} />
+//     </div>
+//   );
+// };
 
 // export default App;
 
-import React, { ChangeEvent, useState, useEffect, FormEvent } from "react";
-import WeatherInfo from './weather-info';
+import React, {
+  ChangeEvent,
+  useState,
+  useEffect,
+  FormEvent,
+  Component,
+} from "react";
+import WeatherInfo from "./weather-info";
 
-export interface Weather{
-   city: string,
-   humidity: number,
-   pressure: number,
-   temp: number,
-   temp_min: number,
-   temp_max: number
+export interface Weather {
+  city: string;
+  humidity: number;
+  pressure: number;
+  temp: number;
+  temp_min: number;
+  temp_max: number;
 }
 
-const App: React.FC = ()=>{
+const App: React.FC = () => {
+  const has = (value: any): value is boolean => !!value;
 
-   const has = (value: any): value is boolean => !!value;
+  const [city, setCity] = useState("London");
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [msgFromChild, setMsgFromChild] = useState("");
 
-   const [city, setCity] = useState('London');
-   const [weather, setWeather] = useState<Weather | null>(null);
-   const [msgFromChild, setMsgFromChild] = useState('');
+  const getMsgFromChild = (msg: string) => setMsgFromChild(msg);
 
-   const getMsgFromChild = (msg: string) => setMsgFromChild(msg);
+  const baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+  const suffix = "&units=imperial&appid=84a11045c5320abf0b94f83a8c259e3b";
 
-   const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q='; 
-   const suffix = "&units=imperial&appid=84a11045c5320abf0b94f83a8c259e3b";
-   
-   const getWeather = async (location: string)=>{
-      const response = await fetch(baseUrl+location+suffix);
-      console.log(response);
-      if(response.status === 200){
-         const jsonWeather = await response.json();
-         const cityTemp: Weather = jsonWeather.main;
-         cityTemp.city = jsonWeather.name;
-         setWeather(cityTemp);
-      }else{
-         setWeather(null);
-      }
+  const getWeather = async (location: string) => {
+    const response = await fetch(baseUrl + location + suffix);
+    console.log(response);
+    if (response.status === 200) {
       const jsonWeather = await response.json();
-      console.log(jsonWeather);
-   }
+      const cityTemp: Weather = jsonWeather.main;
+      cityTemp.city = jsonWeather.name;
+      setWeather(cityTemp);
+    } else {
+      setWeather(null);
+    }
+    const jsonWeather = await response.json();
+    console.log(jsonWeather);
+  };
 
-   useEffect(() => { getWeather(city) }, []);
+  useEffect(() => {
+    getWeather(city);
+  }, []);
 
-   const handleChange = (event: ChangeEvent<HTMLInputElement>)=>{
-      setCity(event.target.value);
-   } 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
 
-   const handleSubmit = (event: FormEvent)=>{
-      event.preventDefault();
-      getWeather(city);
-   }
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    getWeather(city);
+  };
 
-   return(
-      <div>
-         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="enter city" onChange={handleChange}/>
-            <button>Get Weather</button>
-            <h2>City : {city}</h2>
-            {weather && <h2>Temperature: {weather.temp}F</h2>}
-         </form>
-         {msgFromChild}
-         {
-            has(weather)
-            ? <WeatherInfo weather={weather} parentChannel={getMsgFromChild}>
-                <strong>Hello! from parenet component</strong>
-              </WeatherInfo>
-            : <h2>No weather available</h2>
-         }
-      </div>
-   )
-}
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="enter city" onChange={handleChange} />
+        <button>Get Weather</button>
+        <h2>City : {city}</h2>
+        {weather && <h2>Temperature: {weather.temp}F</h2>}
+      </form>
+      {msgFromChild}
+      {has(weather) ? (
+        <WeatherInfo weather={weather} parentChannel={getMsgFromChild}>
+          <strong>Hello! from parenet component</strong>
+        </WeatherInfo>
+      ) : (
+        <h2>No weather available</h2>
+      )}
+    </div>
+  );
+};
 
 export default App;
+// const App: React.FC = () => {
+//   interface Shape {
+//     render(): React.FC;
+//   }
+
+//   interface ShapeRendererProps {
+//     shape: Shape;
+//   }
+
+//   type ShapeProps = (width: number, height: number) => Shape;
+
+//   const Circle: ShapeProps = (width: number, height: number): Shape => {
+//       return
+//   };
+// };
+
+// export default App;
+// interface CompositeProps {
+//   component: any;
+// }
+
+// const Foo = () => <div>Foo</div>;
+
+// const RenderCompositeComponent: React.FC<CompositeProps> = ({
+//   component: Component,
+//   ...props
+// }) => <Component {...props} />;
+
+// <RenderCompositeComponent component={Foo} />;
